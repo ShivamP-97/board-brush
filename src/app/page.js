17ea28f06bb9d1e118e8603a756c718c97b9c577
= <shivampathak19972@gmail.com>
2026-02-16 18:54:49 +0530

    Added Whatsapp Button

--- src/app/page.js
+++ src/app/page.js
@@ -95,6 +95,13 @@ export default function Home() {
         </section>
 
       </main>
+      {/* WhatsApp Floating Button */}
+      <a href="https://wa.me/919999999999"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:scale-110 transition z-50">💬
+      </a>
+
     </>
   );
 }
